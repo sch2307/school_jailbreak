18b346803631763777eda4536997eaaad032aa12
ChoiJuwon <sch2307@naver.com>
2018-08-16 05:40:07 +0000

    professor_assignment_guide.py: Create Car class

--- professor_assignment_guide.py
+++ professor_assignment_guide.py
@@ -39,161 +39,165 @@ import front_wheels
 # =======================================================================
 GPIO.setwarnings(False)
 
-forward_speed = 35
-backward_speed = 32
-turning_angle = 35
 
-delay = 0.0005
+class Car(object):
 
-def first_main():
-    # front_wheels center allignment
-    front_steering.turn_straight()
+    def __init__(self):
+        self.moduleInitialize()
 
-    # forward-assignment
-    rear_wheels_drive.forward_with_speed(30)
-    time.sleep(2) #30-2s
-    rear_wheels_drive.forward_with_speed(60)
-    time.sleep(2) #60-2s
-    rear_wheels_drive.forward_with_speed(90)
-    time.sleep(2) #90-2s
+        # Variable Setup
+        self.forward_speed = 35
+        self.backward_speed = 32
 
-    # backward-assignment
-    rear_wheels_drive.backward_with_speed(30)
-    time.sleep(2) #30-2s
-    rear_wheels_drive.backward_with_speed(60)
-    time.sleep(2) #60-2s
-    rear_wheels_drive.backward_with_speed(90)
-    time.sleep(2) #90-2s
+    def drive_parking(self):
+        self.front_steering.turn_straight()
+        self.rear_wheels_drive.stop()
 
-    # stop
-    rear_wheels_drive.stop()
+    def first_main(self):
+        # front_wheels center allignment
+        self.front_steering.turn_straight()
 
-def lineFollower_main():
-    global turning_angle
+        # forward-assignment
+        self.rear_wheels_drive.forward_with_speed(30)
+        time.sleep(2) #30-2s
+        self.rear_wheels_drive.forward_with_speed(60)
+        time.sleep(2) #60-2s
+        self.rear_wheels_drive.forward_with_speed(90)
+        time.sleep(2) #90-2s
 
-    a_step = 5
-    b_step = 10
-    c_step = 30
-    d_step = 45
+        # backward-assignment
+        self.rear_wheels_drive.backward_with_speed(30)
+        time.sleep(2) #30-2s
+        self.rear_wheels_drive.backward_with_speed(60)
+        time.sleep(2) #60-2s
+        self.rear_wheels_drive.backward_with_speed(90)
+        time.sleep(2) #90-2s
 
-    rear_wheels_drive.forward_with_speed(forward_speed)
+        # stop
+        self.rear_wheels_drive.stop()
 
-    while True:
-        lt_status_now = line_detector.read_digital()
+    def lineFollower_main(self):
+        # lineFollower Setup
+        turning_angle = 35
+        delay = 0.0005
 
-        # Angle calculate
-        if lt_status_now == [0, 0, 1, 0, 0] or lt_status_now == [0, 1, 1, 1, 0]:
-            step = 0
-        elif lt_status_now == [0, 1, 1, 0, 0] or lt_status_now == [0, 0, 1, 1, 0]:
-            step = a_step
-        elif lt_status_now == [0, 1, 0, 0, 0] or lt_status_now == [0, 0, 0, 1, 0]:
-            step = b_step
-        elif lt_status_now == [1, 1, 0, 0, 0] or lt_status_now == [0, 0, 0, 1, 1]:
-            step = c_step
-        elif lt_status_now == [1, 0, 0, 0, 0] or lt_status_now == [0, 0, 0, 0, 1]:
-            step = d_step
-        else:
-            step = 0
+        a_step = 5
+        b_step = 10
+        c_step = 30
+        d_step = 45
 
-        # Direction calculate
-        if lt_status_now == [0, 0, 1, 0, 0]:
-            front_steering.turn(90)
-        # turn right
-        elif sum(lt_status_now[:3]) > sum(lt_status_now[2:]):
-            turning_angle = int(90 - step)
-        # turn left
-        elif sum(lt_status_now[:3]) < sum(lt_status_now[2:]):
-            turning_angle = int(90 + step)
-        elif lt_status_now == [0, 0, 0, 0, 0]:
-            # rgb sensor check
-            rear_wheels_drive.forward_with_speed(forward_speed)
-            time.sleep(0.1)
-            rear_wheels_drive.stop()
-            if line_detector.read_digital() == [0, 0, 0, 0, 0]:
-                time.sleep(0.05)
-                rear_wheels_drive.stop()
-                store_angle = turning_angle
-                turning_angle = 120 if turning_angle < 90 else 60
-                front_steering.turn(turning_angle)
-                time.sleep(0.1)
-                while sum(line_detector.read_digital()) <= 1:
-                    rear_wheels_drive.backward_with_speed(backward_speed)
-                    time.sleep(0.1)
-                rear_wheels_drive.stop()
-                turning_angle = store_angle
-                front_steering.turn(turning_angle)
-                time.sleep(0.1)
-                rear_wheels_drive.forward_with_speed(forward_speed)
-                time.sleep(0.4)
+        self.rear_wheels_drive.forward_with_speed(self.forward_speed)
+
+        while True:
+            lt_status_now = self.line_detector.read_digital()
+
+            # Angle calculate
+            if lt_status_now == [0, 0, 1, 0, 0] or lt_status_now == [0, 1, 1, 1, 0]:
+                step = 0
+            elif lt_status_now == [0, 1, 1, 0, 0] or lt_status_now == [0, 0, 1, 1, 0]:
+                step = a_step
+            elif lt_status_now == [0, 1, 0, 0, 0] or lt_status_now == [0, 0, 0, 1, 0]:
+                step = b_step
+            elif lt_status_now == [1, 1, 0, 0, 0] or lt_status_now == [0, 0, 0, 1, 1]:
+                step = c_step
+            elif lt_status_now == [1, 0, 0, 0, 0] or lt_status_now == [0, 0, 0, 0, 1]:
+                step = d_step
             else:
-                rear_wheels_drive.forward_with_speed(forward_speed)
-                print("COLOR DETECT")
+                step = 0
 
-        front_steering.turn(turning_angle)
-        time.sleep(delay)
+            # Direction calculate
+            if lt_status_now == [0, 0, 1, 0, 0]:
+                self.front_steering.turn(90)
+            # turn right
+            elif sum(lt_status_now[:3]) > sum(lt_status_now[2:]):
+                turning_angle = int(90 - step)
+            # turn left
+            elif sum(lt_status_now[:3]) < sum(lt_status_now[2:]):
+                turning_angle = int(90 + step)
+            elif lt_status_now == [0, 0, 0, 0, 0]:
+                # rgb sensor check
+                self.rear_wheels_drive.forward_with_speed(self.forward_speed)
+                time.sleep(0.1)
+                self.rear_wheels_drive.stop()
+                if self.line_detector.read_digital() == [0, 0, 0, 0, 0]:
+                    time.sleep(0.05)
+                    self.rear_wheels_drive.stop()
+                    store_angle = turning_angle
+                    turning_angle = 120 if turning_angle < 90 else 60
+                    self.front_steering.turn(turning_angle)
+                    time.sleep(0.1)
+                    while sum(self.line_detector.read_digital()) <= 1:
+                        self.rear_wheels_drive.backward_with_speed(self.backward_speed)
+                        time.sleep(0.1)
+                    self.rear_wheels_drive.stop()
+                    turning_angle = store_angle
+                    self.front_steering.turn(turning_angle)
+                    time.sleep(0.1)
+                    self.rear_wheels_drive.forward_with_speed(self.forward_speed)
+                    time.sleep(0.4)
+                else:
+                    self.rear_wheels_drive.forward_with_speed(self.forward_speed)
+                    print("COLOR DETECT")
 
-def moduleInitialize():
-    try:
-        # ================================================================
-        # ULTRASONIC MODULE DRIVER INITIALIZE
-        # ================================================================
-        global distance_detector
-        distance_detector = Ultrasonic_Sensor.Ultrasonic_Avoidance(35)
+            self.front_steering.turn(turning_angle)
+            time.sleep(delay)
 
-        # ================================================================
-        # TRACKING MODULE DRIVER INITIALIZE
-        # ================================================================
-        global line_detector
-        line_detector = Tracking_Sensor.SEN040134_Tracking([16, 18, 22, 40, 32])
+    def moduleInitialize(self):
+        try:
+            # ================================================================
+            # ULTRASONIC MODULE DRIVER INITIALIZE
+            # ================================================================
+            self.distance_detector = Ultrasonic_Sensor.Ultrasonic_Avoidance(35)
 
-        # ================================================================
-        # RGB MODULE DRIVER INITIALIZE
-        # ================================================================
-        global color_getter
-        color_getter = RGB_Sensor.TCS34725()
+            # ================================================================
+            # TRACKING MODULE DRIVER INITIALIZE
+            # ================================================================
+            self.line_detector = Tracking_Sensor.SEN040134_Tracking([16, 18, 22, 40, 32])
 
-        # ================================================================
-        # FRONT WHEEL DRIVER SETUP
-        # ================================================================
-        global front_steering
-        front_steering = front_wheels.Front_Wheels(db='config')
-        front_steering.ready()
+            # ================================================================
+            # RGB MODULE DRIVER INITIALIZE
+            # ================================================================
+            self.color_getter = RGB_Sensor.TCS34725()
 
-        # ================================================================
-        # REAR WHEEL DRIVER SETUP
-        # ================================================================
-        global rear_wheels_drive
-        rear_wheels_drive = rear_wheels.Rear_Wheels(db='config')
-        rear_wheels_drive.ready()
+            # ================================================================
+            # FRONT WHEEL DRIVER SETUP
+            # ================================================================
+            self.front_steering = front_wheels.Front_Wheels(db='config')
+            self.front_steering.ready()
 
-        # ================================================================
-        # SET LIMIT OF TURNING DEGREE
-        # ===============================================================
-        front_steering.turning_max = 35
+            # ================================================================
+            # REAR WHEEL DRIVER SETUP
+            # ================================================================
+            self.rear_wheels_drive = rear_wheels.Rear_Wheels(db='config')
+            self.rear_wheels_drive.ready()
 
-        # ================================================================
-        # SET FRONT WHEEL CENTOR ALLIGNMENT
-        # ================================================================
-        front_steering.turn_straight()
+            # ================================================================
+            # SET LIMIT OF TURNING DEGREE
+            # ===============================================================
+            self.front_steering.turning_max = 35
 
-        # ================================================================
-        # DISABLE RGB MODULE INTERRUPTION
-        # ================================================================
-        color_getter.set_interrupt(False)
+            # ================================================================
+            # SET FRONT WHEEL CENTOR ALLIGNMENT
+            # ================================================================
+            self.front_steering.turn_straight()
 
-    except:
-        print("MODULE INITIALIZE ERROR")
-        print("CONTACT TO Kookmin Univ. Teaching Assistant")
+            # ================================================================
+            # DISABLE RGB MODULE INTERRUPTION
+            # ================================================================
+            self.color_getter.set_interrupt(False)
+
+        except:
+            print("MODULE INITIALIZE ERROR")
+            print("CONTACT TO Kookmin Univ. Teaching Assistant")
 
 
 if __name__ == "__main__":
     try:
-        moduleInitialize()
-        #first_main()
-        lineFollower_main()
+        car = Car()
+        car.first_main() #1st_assignment
+        car.lineFollower_main() #2nd-3rd assignment
 
     except KeyboardInterrupt:
         # when the Ctrl+C key has been pressed,
         # the moving object will be stopped
-        rear_wheels_drive.stop()
-        front_steering.turn_straight()
+        car.drive_parking()
